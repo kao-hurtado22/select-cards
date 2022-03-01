8930284f8e7bbdc2fbe9675dfd986c3ab4e60b5f
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-03-01 06:03:03 +0000

    select cards terminado

--- src/app.js
+++ src/app.js
@@ -1,49 +1,46 @@
 window.onload = () => {
   let suits = ["diams", "hearts", "clubs", "spades"];
-  let ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  let objOfCards = [];
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  let oderCards = [];
 
   let buttonDraw = document.querySelector(".draw-button");
   buttonDraw.addEventListener("click", myFunctionCards);
 
   function myFunctionCards() {
-    objOfCards = [];
+    oderCards = [];
     let myCard = document.querySelector(".container");
     if (myCard.childNodes.length !== 0) {
       myCard.innerHTML = "";
     }
     let inputValueCards = parseInt(
       document.getElementById("numbercards").value
-    );
-
+    ); //estoy llamando al valor del input que coloca el usuario
     if (inputValueCards === 0) {
       alert("Coloca un numero mayor a 0");
     } else if (inputValueCards !== 0) {
       for (let i = 0; i < inputValueCards; i++) {
         let objElementsCard = {
           suits: suits[Math.floor(Math.random() * suits.length)],
-          ranks: ranks[Math.floor(Math.random() * ranks.length)]
+          numbers: numbers[Math.floor(Math.random() * numbers.length)]
         };
-
-        objOfCards.push(objElementsCard);
+        oderCards.push(objElementsCard);
       }
-
       if (myCard.childNodes.length === 0) {
-        for (let element of objOfCards) {
-          if (element.ranks === 1) {
-            element.ranks = "A";
+        for (let element of oderCards) {
+          if (element.numbers === 1) {
+            element.numbers = "A";
           }
-          if (element.ranks === 11) {
-            element.ranks = "J";
+          if (element.numbers === 11) {
+            element.numbers = "J";
           }
-          if (element.ranks === 12) {
-            element.ranks = "Q";
+          if (element.numbers === 12) {
+            element.numbers = "Q";
           }
-          if (element.ranks === 13) {
-            element.ranks = "K";
+          if (element.numbers === 13) {
+            element.numbers = "K";
           }
           let myDiv = document.createElement("div");
-          let contentDiv = document.createTextNode(`${element.ranks}`);
+          let contentDiv = document.createTextNode(`${element.numbers}`);
           myDiv.className = `card ${element.suits}`;
           myDiv.appendChild(contentDiv);
           myCard.appendChild(myDiv);
@@ -51,66 +48,76 @@ window.onload = () => {
       }
     }
   }
-
-  const bubbleSort = () => {
-    let myContainerDiv = document.querySelector(".contenedor-listas-sort");
+  const selectSort = () => {
+    let myContainerDiv = document.querySelector(".list");
     if (myContainerDiv.childNodes.length !== 0) {
       myContainerDiv.innerHTML = "";
     }
-    let wall = objOfCards.length - 1;
-    while (wall > 0) {
-      let index = 0;
-      while (index < wall) {
-        if (objOfCards[index].ranks === "A") {
-          objOfCards[index].ranks = 1;
-        }
-        if (objOfCards[index].ranks === "J") {
-          objOfCards[index].ranks = 11;
-        }
-        if (objOfCards[index].ranks === "Q") {
-          objOfCards[index].ranks = 12;
-        }
-        if (objOfCards[index].ranks === "K") {
-          objOfCards[index].ranks = 13;
-        }
-
-        if (objOfCards[index].ranks > objOfCards[index + 1].ranks) {
-          let aux = objOfCards[index].ranks;
-          objOfCards[index].ranks = objOfCards[index + 1].ranks;
-          objOfCards[index + 1].ranks = aux;
-        }
-        index++;
+    let min = 0;
+    while (min < oderCards.length - 1) {
+      if (oderCards[min].numbers === "A") {
+        oderCards[min].numbers = 1;
       }
-      wall--;
+      if (oderCards[min].numbers === "J") {
+        oderCards[min].numbers = 11;
+      }
+      if (oderCards[min].numbers === "Q") {
+        oderCards[min].numbers = 12;
+      }
+      if (oderCards[min].numbers === "K") {
+        oderCards[min].numbers = 13;
+      }
+      for (let i = min + 1; i < oderCards.length; i++) {
+        if (oderCards[i].numbers === "A") {
+          oderCards[i].numbers = 1;
+        }
+        if (oderCards[i].numbers === "J") {
+          oderCards[i].numbers = 11;
+        }
+        if (oderCards[i].numbers === "Q") {
+          oderCards[i].numbers = 12;
+        }
+        if (oderCards[i].numbers === "K") {
+          oderCards[i].numbers = 13;
+        }
+        if (oderCards[min].numbers > oderCards[i].numbers) {
+          let aux = oderCards[min].numbers;
+          oderCards[min].numbers = oderCards[i].numbers;
+          oderCards[i].numbers = aux;
+        }
+      }
+      min++;
+
       let myUl = document.createElement("ul");
       let liNumber = document.createElement("li");
-      liNumber.innerHTML = `${index - 1}:`;
+      liNumber.innerHTML = `${min - 1}:`;
       myUl.appendChild(liNumber);
       myUl.className = "lista-containers-cards-sort";
       myContainerDiv.appendChild(myUl);
-      for (let element of objOfCards) {
-        if (element.ranks === 1) {
-          element.ranks = "A";
+      // Este for itera cada elemento del objeto para imprimir la carta en el html
+      for (let element of oderCards) {
+        if (element.numbers === 1) {
+          element.numbers = "A";
         }
-        if (element.ranks === 11) {
-          element.ranks = "J";
+        if (element.numbers === 11) {
+          element.numbers = "J";
         }
-        if (element.ranks === 12) {
-          element.ranks = "Q";
+        if (element.numbers === 12) {
+          element.numbers = "Q";
         }
-        if (element.ranks === 13) {
-          element.ranks = "K";
+        if (element.numbers === 13) {
+          element.numbers = "K";
         }
         let myLi = document.createElement("li");
-        let contentLi = document.createTextNode(`${element.ranks}`);
+        let contentLi = document.createTextNode(`${element.numbers}`);
         myLi.className = `card ${element.suits}`;
         myLi.appendChild(contentLi);
         myUl.appendChild(myLi);
       }
     }
-    return objOfCards;
+    return oderCards;
   };
 
   let sortButton = document.querySelector(".sort-button");
-  sortButton.addEventListener("click", bubbleSort);
+  sortButton.addEventListener("click", selectSort);
 };
